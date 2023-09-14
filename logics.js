39e285f213cb416c614272440d8d41f1ac7ca29a
Klainus <106882227+Klainus@users.noreply.github.com>
2023-09-14 22:51:59 +0200

    added button to print out the last variable from ages array

--- logics.js
+++ logics.js
@@ -3,3 +3,9 @@ var float = 14.7
 var string = "This is a string"
 var bool = true 
 
+function array() {
+    
+    var ages = [9, 17, 25, 39, 71]
+    var lastAge = ages[ages.length - 1]
+    alert(lastAge)
+}
